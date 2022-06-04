0914618c1f1597b58306e69276b4774b1f1d00a0
Che <43485962+c-git@users.noreply.github.com>
2022-06-04 16:55:38 -0400

    Add test to ensure no folder is created for logs in only_std mode

--- tests/test_log.py
+++ tests/test_log.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from unittest import TestCase
 
 from opylib.files_folders import mkdir
-from opylib.log import log_exception, setup_log
+from opylib.log import discard_handlers, log, log_exception, setup_log
 from tests.config_tests import TestsConfig
 
 
@@ -18,13 +18,17 @@ class Test(TestCase):
                          msg=f'"{os.path.abspath(fn)}" should NOT exist')
 
     def test_setup_log_and_change_dir(self):
-        # Test to confirm that error file is created in the correct place even
-        # if the working directory gets changed during execution
+        """
+        Test to confirm that error file is created in the correct place even
+        if the working directory gets changed during execution. Also test
+        that entries are written to the log file
+        """
 
         if not TestsConfig.get_instance().no_skip:
             return
 
-        base_log_dir, log_fn, err_log_fn = self.start_logging(False)
+        base_log_dir, log_fn, err_log_fn = self.start_logging(
+            only_std_out=False, test_num=1)
 
         # Ensure log files are created
         self.assert_exists(log_fn)
@@ -54,8 +58,37 @@ class Test(TestCase):
         shutil.rmtree(base_log_dir)
         self.assert_not_exists(base_log_dir)
 
-    def start_logging(self, only_std_out: bool) -> Tuple[str, str, str]:
-        base_log_test_dir = 'testLog/'
+    def test_no_files_created_in_only_std(self):
+        if not TestsConfig.get_instance().no_skip:
+            return
+
+        base_log_dir, log_fn, err_log_fn = self.start_logging(only_std_out=True,
+                                                              test_num=2)
+
+        # Ensure log files are NOT created
+        self.assert_not_exists(base_log_dir)
+
+        # Write a message to the log
+        log('Test Message')
+
+        # Register an exception to create the error logger
+        log_exception(Exception('NOT AN ERROR PART OF TESTS'))
+
+        # Ensure log files are NOT created
+        self.assert_not_exists(base_log_dir)
+
+    def start_logging(self, *, only_std_out: bool, test_num: int) -> \
+            Tuple[str, str, str]:
+        """
+        Clears out any existing handler and registers new ones. Ensures base
+        both files names returns are in base_dir
+        :param only_std_out: Control if only std output should be used
+        :param test_num:A number separate multiple test are run simultaneously
+        :return:
+        """
+        discard_handlers()
+
+        base_log_test_dir = f'testLog{test_num}/'
         log_fn = base_log_test_dir + 'logfile.log'
         err_log_fn = base_log_test_dir + 'eTestLog/ERRORS.log'
 
