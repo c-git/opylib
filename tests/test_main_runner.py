517ccade2848619f08478c9b5d3dc8b1dce01211
One <43485962+c-git@users.noreply.github.com>
2023-12-28 14:37:36 -0500

    Split main_runner tests

--- tests/test_main_runner.py
+++ tests/test_main_runner.py
@@ -16,9 +16,11 @@ def test_main_with_exception():
 
 
 class Test(TestCase):
-    def test_main_runner(self):
+    def test_main_runner_normal(self):
         with self.assertLogs():
             main_runner(test_main, should_complete_notify=False)
+
+    def test_main_runner_logs_to_error(self):
         if TestsConfig.get_instance().no_skip:
             with self.assertLogs(level=logging.ERROR):
                 main_runner(test_main_with_exception)
