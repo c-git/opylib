7f0438e788d6577fe3ab6e6437fa1b4b0d02cfe4
One <43485962+c-git@users.noreply.github.com>
2024-01-03 13:21:00 -0500

    Remove argument it's hard to use and delay before error means delay is only sed when needed

--- src/opylib/notify.py
+++ src/opylib/notify.py
@@ -11,56 +11,54 @@ if sys.platform.startswith('linux'):
         from google.colab import output
 
 
-        def make_sound(_):
+        def make_sound():
             output.eval_js(
                 'new Audio('
                 '"https://upload.wikimedia.org/wikipedia/commons/0/05'
                 '/Beep-09.ogg").play()')
 
 
-        def notify_err(_):
+        def notify_err():
             make_sound()
 
     except ModuleNotFoundError:
-        def make_sound(sleep_time=3):
+        def make_sound():
             os.system('spd-say "Process finished"')
-            sleep(sleep_time)
 
 
-        def notify_err(sleep_time=0):
+        def notify_err():
+            sleep(2)  # Delay before running to late end of program finish playing
             os.system('spd-say "Errors detected"')
-            sleep(sleep_time)
 
 elif sys.platform.startswith('darwin'):
-    def make_sound(sleep_time=3):
+    def make_sound():
         os.system('say "Process finished"')
-        sleep(sleep_time)
 
 
-    def notify_err(sleep_time=0):
+    def notify_err():
+        sleep(2)  # Delay before running to late end of program finish playing
         os.system('say "Errors detected"')
-        sleep(sleep_time)
 
 elif sys.platform.startswith('win') or sys.platform.startswith('cygwin'):
     import winsound
 
 
-    def make_sound(_=None):
+    def make_sound():
         for i in range(1, 5):
             winsound.Beep(i * 400, 200)
         for i in range(5, 1, -1):
             winsound.Beep(i * 400, 200)
 
 
-    def notify_err(_=None):
+    def notify_err():
         for i in range(2):
             winsound.Beep(2000, 500)
             winsound.Beep(400, 200)
 else:
-    def make_sound(_=None):
+    def make_sound():
         log(Exception(f'No Sound because unexpected OS: {sys.platform}'),
             logging.WARN)
 
 
-    def notify_err(_=None):
+    def notify_err():
         make_sound()
